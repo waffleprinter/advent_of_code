405cb41d2d0214a0dcbe5a1ffb964dc33bcf1dcb
waffleprinter <dwamsie@gmail.com>
2024-02-25 10:05:42 -0500

    Reformated code and split code into different parts

--- Code/2015 Day 6 Part 1 - Probably a Fire Hazard.py
+++ Code/2015 Day 6 Part 1 - Probably a Fire Hazard.py
@@ -1,4 +1,4 @@
-def update_grid_part_1(instruction, start, end):
+def update_grid(instruction, start, end):
     for r in range(start[0], end[0] + 1):
         for c in range(start[1], end[1] + 1):
             if instruction == 'turn on':
@@ -11,22 +11,9 @@ def update_grid_part_1(instruction, start, end):
                 grid[r][c] = not grid[r][c]
 
 
-def update_grid_part_2(instruction, start, end):
-    for r in range(start[0], end[0] + 1):
-        for c in range(start[1], end[1] + 1):
-            if instruction == 'turn on':
-                grid[r][c] += 1
-            
-            elif instruction == 'turn off':
-                grid[r][c] -= 1 if grid[r][c] > 0 else 0
+grid = [[False for i in range(1000)] for i in range(1000)]
 
-            else:
-                grid[r][c] += 2
-
-
-grid = [[0 for i in range(1000)] for i in range(1000)]
-
-with open(r"C:\Users\jccpa\advent_of_code\Inputs\2015 Day 6 - Probably a Fire Hazard.txt", 'r') as f:
+with open(r"Inputs\2015 Day 6 - Probably a Fire Hazard.txt", 'r') as f:
     for line in f.readlines():
         instruction = None
 
@@ -47,7 +34,7 @@ with open(r"C:\Users\jccpa\advent_of_code\Inputs\2015 Day 6 - Probably a Fire Ha
         start = list(map(int, line[0].split(',')))
         end = list(map(int, line[2].split(',')))
 
-        update_grid_part_2(instruction, start, end)
+        update_grid(instruction, start, end)
 
 print(sum(sum(row) for row in grid))
         
